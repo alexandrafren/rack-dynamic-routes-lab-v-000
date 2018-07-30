9a24123463cbdf2176f028abfa7f2a8dbd580977
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-30 04:07:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,12 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     if req.path.match(/items/)
+      search_item = resp.params["item"]
+      if @@items.include?(search_item)
+        resp.write "#{search_item.price}"
+      else
+        resp.write "Item not found"
+      end
     else
       resp.status = 404
       resp.write "Route not found"
